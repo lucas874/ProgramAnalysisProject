6549b7f659928a4afb457037e1eb27276fd58433
lucas874 <lsc500@outlook.dk>
2023-11-21 18:49:36 +0100

    more examples...

--- course-02242-examples/src/executables/java/eu/bogoe/dtu/exceptional/Sorting.java
+++ course-02242-examples/src/executables/java/eu/bogoe/dtu/exceptional/Sorting.java
@@ -106,6 +106,125 @@ public class Sorting {
         return -1;
     }
 
+    @Case // https://rosettacode.org/wiki/Sorting_algorithms/Gnome_sort#Java
+    public static void gnomeSort(int[] a) {
+        int i=1;
+        int j=2;
+    
+        while(i < a.length) {
+            if ( a[i-1] <= a[i] ) {
+            i = j; j++;
+            } else {
+            int tmp = a[i-1];
+            a[i-1] = a[i];
+            a[i--] = tmp;
+            i = (i==0) ? j++ : i;
+            }
+        }
+    }
+    @Case // https://rosettacode.org/wiki/Sorting_algorithms/Cycle_sort#Java
+    static int cycleSort(int[] a) {
+        int writes = 0;
+
+        for (int cycleStart = 0; cycleStart < a.length - 1; cycleStart++) {
+            int val = a[cycleStart];
+
+            // count the number of values that are smaller than val
+            // since cycleStart
+            int pos = cycleStart;
+            for (int i = cycleStart + 1; i < a.length; i++)
+                if (a[i] < val)
+                    pos++;
+
+            // there aren't any
+            if (pos == cycleStart)
+                continue;
+
+            // skip duplicates
+            while (val == a[pos])
+                pos++;
+
+            // put val into final position
+            int tmp = a[pos];
+            a[pos] = val;
+            val = tmp;
+            writes++;
+
+            // repeat as long as we can find values to swap
+            // otherwise start new cycle
+            while (pos != cycleStart) {
+                pos = cycleStart;
+                for (int i = cycleStart + 1; i < a.length; i++)
+                    if (a[i] < val)
+                        pos++;
+
+                while (val == a[pos])
+                    pos++;
+
+                tmp = a[pos];
+                a[pos] = val;
+                val = tmp;
+                writes++;
+            }
+        }
+        return writes;
+    }
+
+/*     @Case // https://rosettacode.org/wiki/Sorting_algorithms/Heapsort#Java
+    public static void heapSort(int[] a){
+        int count = a.length;
+    
+        //first place a in max-heap order
+        heapify(a, count);
+    
+        int end = count - 1;
+        while(end > 0){
+            //swap the root(maximum value) of the heap with the
+            //last element of the heap
+            int tmp = a[end];
+            a[end] = a[0];
+            a[0] = tmp;
+            //put the heap back in max-heap order
+            siftDown(a, 0, end - 1);
+            //decrement the size of the heap so that the previous
+            //max value will stay in its proper place
+            end--;
+        }
+    }
+    
+    public static void heapify(int[] a, int count){
+        //start is assigned the index in a of the last parent node
+        int start = (count - 2) / 2; //binary heap
+    
+        while(start >= 0){
+            //sift down the node at index start to the proper place
+            //such that all nodes below the start index are in heap
+            //order
+            siftDown(a, start, count - 1);
+            start--;
+        }
+        //after sifting down the root all nodes/elements are in heap order
+    }
+    
+    public static void siftDown(int[] a, int start, int end){
+        //end represents the limit of how far down the heap to sift
+        int root = start;
+    
+        while((root * 2 + 1) <= end){      //While the root has at least one child
+            int child = root * 2 + 1;           //root*2+1 points to the left child
+            //if the child has a sibling and the child's value is less than its sibling's...
+            if(child + 1 <= end && a[child] < a[child + 1])
+                child = child + 1;           //... then point to the right child instead
+            if(a[root] < a[child]){     //out of max-heap order
+                int tmp = a[root];
+                a[root] = a[child];
+                a[child] = tmp;
+                root = child;                //repeat to continue sifting down the child now
+            }else
+                return;
+        }
+    } */
+
     public static void printArray(int[] array) {
         for(int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
